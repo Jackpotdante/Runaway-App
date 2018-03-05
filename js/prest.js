0af235ac2e577cfe3dd6c55c38c326e30882d305
Johan Augustsson <j.augustsson80@gmail.com>
2018-03-05 19:15:46 +0100

    totalsträcka uppdaterad och fixad i prestaioner. userLogedin blir uppdaterad med den infon

--- js/prest.js
+++ js/prest.js
@@ -2,73 +2,74 @@ window.addEventListener("load", function (){
 	let totalLength = 0;
 	let longestDistance = 0;
 
-	let btnLoadTracks = document.getElementById('btnLoadTracks');
-	btnLoadTracks.addEventListener('click', function(event){
-		getTracksFromUser();
-	});
-
-	function createPrest(place, length, time, date, routeId, share){
+	function createPrest(dataForRace){
 		let prestList = document.getElementsByClassName("containerPrestation")
 		let newDiv = document.createElement("div");
 		let newPlace = document.createElement("span");
 		let newLength = document.createElement("span");
 		let newTime = document.createElement("span");
+		let newName = document.createElement("span");
 		let newDate = document.createElement("span");
 		let newSwitchBox = document.createElement("div");
 		let newSwitch = document.createElement("div");
 		let newSwitchCircle = document.createElement("div");
-		let image = document.createElement("img");
+		//let image = document.createElement("img");
 		let spanTotalLength = document.getElementById("spanTotalLength");
 		let spanLongestDist = document.getElementById("spanLongestDist");
-		let thisDistance = length;
-
+		let thisDistance = dataForRace.length;
+		newName.className="nameOfTrack";
 
 
 		totalLength += thisDistance;
+		currentUser.totalLength = totalLength; //sparar totalsträcka till user objecktet
+	  currentUser.longestRun = longestDistance; //sparar totalsträcka till user objecktet
+
 		spanTotalLength.innerText = `Total längd: ${totalLength}km`;
 		spanLongestDist.innerText = `Längst sträcka: ${longestDistance}km`;
 
 		if(longestDistance < thisDistance){
 			longestDistance = thisDistance;
 			spanLongestDist.innerText = `Längst sträcka: ${longestDistance}km`;
+			currentUser.longestRun = thisDistance;
 		}
 
 
 
 
-		newPlace.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${place}`
-		newLength.innerHTML = `&#128095 ${length}km`;
-		newTime.innerHTML = `<i class="fas fa-stopwatch"></i> ${time}`;
-		newDate.innerHTML = `<i class="far fa-calendar"></i> ${date}`;
-		image.src = "#";
+		newPlace.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${dataForRace.place}`
+		newLength.innerHTML = `&#128095 ${dataForRace.length}km`;
+		newTime.innerHTML = `<i class="fas fa-stopwatch"></i> ${dataForRace.time}`;
+		newDate.innerHTML = `<i class="far fa-calendar"></i> ${dataForRace.date}`;
+		//image.src = "#";
 		newSwitchBox.classList.add("switchBox");
 		newSwitch.classList.add("switch");
 		newSwitchCircle.classList.add("switchCircle");
 		newSwitchBox.innerText = "Dela: ";
-
+		newName.innerHTML = dataForRace.name;
 
 		prestList[0].appendChild(newDiv);
 		newDiv.appendChild(newPlace);
 		newDiv.appendChild(newLength);
 		newDiv.appendChild(newTime);
 		newDiv.appendChild(newDate);
-		newDiv.appendChild(image);
+		newDiv.appendChild(newName);
+		//newDiv.appendChild(image);
 		newDiv.appendChild(newSwitchBox);
 		newSwitchBox.appendChild(newSwitch);
 		newSwitch.appendChild(newSwitchCircle);
 
 
-		if(share == true)newSwitch.classList.add("active");
+		if(dataForRace.share == true)newSwitch.classList.add("active");
 
 
 		newSwitchBox.addEventListener("click", function(){
 			newSwitch.classList.toggle("active");
 
 			if(newSwitch.classList[1] == "active"){
-				db.ref("rundor/" + routeId).update({share: true})
+				db.ref("rundor/" + dataForRace.raceId).update({share: true})
 			}
 			else{
-				db.ref("rundor/" + routeId).update({share: false})
+				db.ref("rundor/" + dataForRace.raceId).update({share: false})
 			}
 		})
 
@@ -79,16 +80,24 @@ window.addEventListener("load", function (){
 		db.ref("rundor/").on("child_added", function(snapshot, prevChildKey){
 			let data = snapshot.val();
 			let route = snapshot.key;
+			let trackId = data.trackid;
 
-			//ta bort denna när vi fått ordning på inloggad användre.
-			createPrest(data.place, data.length, data.time, data.date, route, data.share);
+			if(data.user == currentUser.uid){
 
-			if(data.user == currentUser){ //används när vi väl fått ordning på inloggad användre
-				console.log("inside currne");
-				createPrest(data.place, data.length, data.time, data.date, route, data.share);
+				let dataForRace = {
+					place : runningTracks[trackId].place, //runningTracks kommer från cardsMap
+					length : runningTracks[trackId].length,
+					name: runningTracks[trackId].name,
+					time : data.time,
+					date : data.date,
+					raceId : route,
+					share : data.share
+				}
+				createPrest(dataForRace);
 			}
 		})
+
 	}
 
-
+	getTracksFromUser();
 })
